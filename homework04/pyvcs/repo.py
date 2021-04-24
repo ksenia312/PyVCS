089997929a805a26ecf71bfdad5e6a381f229d30
ksenia312 <ksynikitina.02@mail.ru>
2021-04-24 13:33:27 +0300

    repo

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,10 +4,44 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    name = os.getenv("GIT_DIR", ".pyvcs")
+    workdir = pathlib.Path(workdir)
+
+    while pathlib.Path(workdir.absolute().root) != workdir.absolute():
+        if (workdir / name).is_dir():
+            return workdir / name
+        workdir = workdir.parent
+
+    if (workdir / name).is_dir():
+        return workdir / name
+    raise Exception("Not a git repository")
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    name = os.getenv("GIT_DIR", ".pyvcs")
+    workdir = pathlib.Path(workdir)
+    path = workdir / name
+
+    if workdir.is_file():
+        raise Exception(f"{workdir} is not a directory")
+    os.makedirs(path / "refs" / "heads", exist_ok=True)
+    os.makedirs(path / "refs" / "tags", exist_ok=True)
+
+    (path / "objects").mkdir()
+
+    with (path / "config").open("w") as f:
+        f.write(
+            "[core]\n\t"
+            "repositoryformatversion = 0\n\t"
+            "filemode = true\n\t"
+            "bare = false\n\t"
+            "logallrefupdates = false\n"
+        )
+
+    with (path / "HEAD").open("w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with (path / "description").open("w") as f:
+        f.write("Unnamed pyvcs repository.\n")
+
+    return path
